915ceddaef0c0249bb7cbb15610e9b76e88214cd
JacobKatzIA <jacke.katz@gmail.com>
2025-06-02 11:56:40 +0200

    Skapat ny komponent - SavedMemesModal
    Flyttade Modal för sparade memes till en separat komponent

--- generator/src/components/MemeList.js
+++ generator/src/components/MemeList.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import MemeCreator from "./MemeCreator"
 import RandomMeme from "./ReditMeme";
+import SavedMemesModal from "./SavedMemesModal";
 
 function MemeTemplate() {
     const [memes, setMemes] = useState([]);
@@ -67,43 +68,7 @@ function MemeTemplate() {
             ))}
 
 
-            <div className="modal fade" id="savedMemesModal" tabIndex="-1" aria-hidden="true">
-                <div className="modal-dialog modal-lg modal-dialog-centered">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title">Sparade Memes</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            {savedMemes.length === 0 ? (
-                                <p>Inga sparade memes ännu.</p>
-                            ) : (
-                                <div className="row">
-                                    {savedMemes.map((url, index) => (
-                                        <div className="col-md-4 mb-3" key={index}>
-                                            <div className="card h-100 text-center">
-                                                <img 
-                                                    src={url} 
-                                                    alt={`Saved meme ${index + 1}`}
-                                                    className="card-img-top mx-auto d-block"
-                                                />
-                                                <div className="card-body">
-                                                    <button 
-                                                        className="btn btn-danger" 
-                                                        onClick={() => deleteMeme(index)}
-                                                    > 
-                                                        Ta bort
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <SavedMemesModal savedMemes={savedMemes} onDelete={deleteMeme} />
         </div>
     );
 }
