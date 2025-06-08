a3cb1f2c4545faaf6dbf19dbd21bbe8fe0ef81f3
JacobKatzIA <jacke.katz@gmail.com>
2025-06-08 13:21:49 +0200

    Kommentarer i SavedMemesModal
    Kommenterat filen som visar sparade memes (och som tillåter användaren att ta bort memes som sparats med local storage)

--- generator/src/components/SavedMemesModal.js
+++ generator/src/components/SavedMemesModal.js
@@ -1,19 +1,30 @@
+// Importerar React 
 import React from "react";
 
+// Komponent som visar en modal med sparade memes
+// Tar emot två props: "savedMemes" (en array av URL:er) och "onDelete" (en funktion som låter användaren ta bort en meme)
 function SavedMemesModal({ savedMemes, onDelete }) {
     return (
         <div className="modal fade" id="savedMemesModal" tabIndex="-1" aria-hidden="true">
+
             <div className="modal-dialog modal-lg modal-dialog-centered">
+
                 <div className="modal-content">
+
                     <div className="modal-header">
                         <h5 className="modal-title">Sparade Memes</h5>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
+
                     <div className="modal-body">
+
+                        {/* Ifall det inte finns några sparade memes */}
                         {savedMemes.length === 0 ? (
                             <p>Inga sparade memes ännu.</p>
                         ) : (
                             <div className="row">
+                                
+                                {/* Loopar igenom varje sparad meme URL */}
                                 {savedMemes.map((url, index) => (
                                     <div className="col-md-4 mb-3" key={index}>
                                         <div className="card h-100 text-center">
@@ -22,6 +33,8 @@ function SavedMemesModal({ savedMemes, onDelete }) {
                                                 alt={`Saved meme ${index + 1}`}
                                                 className="card-img-top mx-auto d-block"
                                             />
+
+                                            {/* Knapp för att ta bort sparad meme */}
                                             <div className="card-body">
                                                 <button 
                                                     className="btn btn-danger" 
